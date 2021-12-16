40c0465619cbee3d97f886003f5d80d0adf7a804
Darren Robinson <darren@darrenjrobinson.com>
2021-12-16 17:32:43 +1100

    Initial Commit
    Initial commit PKCE PS Module

---
+++ PKCE.psm1
@@ -0,0 +1,165 @@
+Function New-PKCE {
+    <#
+    .SYNOPSIS
+    Generate OAuth 2.0 Proof Key for Code Exchange (PKCE) 'code_challenge' and 'code_verifier' for use with an OAuth2 Authorization Code Grant flow 
+
+    .DESCRIPTION
+    Proof Key for Code Exchange (PKCE) is a mechanism, typically used together with an OAuth2 Authorization Code Grant flow to provide an enhanced level of security when authenticating to an Identity Provider (IDP) to get an access token.
+
+    .PARAMETER codeVerifier
+    (optional) ClientID and ClientSecret of the Azure AD registered application with the necessary permissions for MSUser v3.
+
+    .PARAMETER length
+    (optional) Length of the code verifier to generate
+
+    .EXAMPLE
+    New-PKCE
+
+    .EXAMPLE 
+    Generate the code challenge for a specific code verifier
+    New-PKCE -codeVerifier 'yfQ3wNRAyimC2qFc0wXI04u6pb2vRWRfUGdbcILFYOxqC1iJ84dSU0uCsVsHoMuv4Mbu5kmQxd3sZspfnPotrIPx1A9DOVmY3ahcKTjJ5xoGz95A7J8zSw86HW5eZpE'
+
+    .EXAMPLE
+    Specify the length of the code verifier to generate
+    New-PKCE -length 99
+
+    .LINK
+    http://darrenjrobinson.com/
+
+    #>
+
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [ValidatePattern('(?# Code Verifier can only contain alphanumeric characters and . ~ - _)^[a-zA-Z0-9-._~]+$')][string]$codeVerifier,
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [int]$length = 128
+    )
+
+
+    if ($length -gt 128 -or $length -lt 43) {
+        Write-Warning "Code Verifier length must be of 43 to 128 characters in length (inclusive)."
+        exit 
+    }
+
+    if ($codeVerifier) {
+        if ($codeVerifier.Length -gt 128 -or $codeVerifier.Length -lt 43) {
+            Write-Warning "Code Verifier length must be of 43 to 128 characters in length (inclusive)."
+            exit 
+        }  
+    }
+
+    $federationTemplate = [pscustomobject][ordered]@{  
+        code_verifier  = $null  
+        code_challenge = $null   
+    }  
+        
+    if ($codeVerifier) {
+        $hashAlgo = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
+        $hash = $hashAlgo.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($codeVerifier))
+        $b64Hash = [System.Convert]::ToBase64String($hash)
+        $code_challenge = $b64Hash.Substring(0, 43)
+        $code_challenge = $code_challenge.Replace("/","_")
+        $code_challenge = $code_challenge.Replace("+","-")
+        $code_challenge = $code_challenge.Replace("=","")
+
+        $pkceChallenges = $federationTemplate.PsObject.Copy()
+        $pkceChallenges.code_challenge = $code_challenge 
+        $pkceChallenges.code_verifier = $codeVerifier 
+
+        return $pkceChallenges 
+    }
+    else {
+        # PKCE Code verifier. Random alphanumeric string used on the client side
+        # From the ASCII Table in Decimal A-Z a-z 0-9
+        $codeVerifier = -join (((48..57) * 4) + ((65..90) * 4) + ((97..122) * 4) | Get-Random -Count $length | ForEach-Object { [char]$_ })
+
+        $hashAlgo = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
+        $hash = $hashAlgo.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($codeVerifier))
+        $b64Hash = [System.Convert]::ToBase64String($hash)
+        $codeChallenge = $b64Hash.Substring(0, 43)
+        
+        $pkceChallenges = $federationTemplate.PsObject.Copy()
+        $pkceChallenges.code_challenge = $codeChallenge
+        $pkceChallenges.code_verifier = $codeVerifier 
+
+        return $pkceChallenges 
+    }
+}
+
+Export-ModuleMember -Function 'New-PKCE'
+
+
+# SIG # Begin signature block
+# MIINSwYJKoZIhvcNAQcCoIINPDCCDTgCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUd0tfmx5QS6GRlTiTxauo+xkj
+# 7UmgggqNMIIFMDCCBBigAwIBAgIQBAkYG1/Vu2Z1U0O1b5VQCDANBgkqhkiG9w0B
+# AQsFADBlMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
+# VQQLExB3d3cuZGlnaWNlcnQuY29tMSQwIgYDVQQDExtEaWdpQ2VydCBBc3N1cmVk
+# IElEIFJvb3QgQ0EwHhcNMTMxMDIyMTIwMDAwWhcNMjgxMDIyMTIwMDAwWjByMQsw
+# CQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cu
+# ZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQg
+# Q29kZSBTaWduaW5nIENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+# +NOzHH8OEa9ndwfTCzFJGc/Q+0WZsTrbRPV/5aid2zLXcep2nQUut4/6kkPApfmJ
+# 1DcZ17aq8JyGpdglrA55KDp+6dFn08b7KSfH03sjlOSRI5aQd4L5oYQjZhJUM1B0
+# sSgmuyRpwsJS8hRniolF1C2ho+mILCCVrhxKhwjfDPXiTWAYvqrEsq5wMWYzcT6s
+# cKKrzn/pfMuSoeU7MRzP6vIK5Fe7SrXpdOYr/mzLfnQ5Ng2Q7+S1TqSp6moKq4Tz
+# rGdOtcT3jNEgJSPrCGQ+UpbB8g8S9MWOD8Gi6CxR93O8vYWxYoNzQYIH5DiLanMg
+# 0A9kczyen6Yzqf0Z3yWT0QIDAQABo4IBzTCCAckwEgYDVR0TAQH/BAgwBgEB/wIB
+# ADAOBgNVHQ8BAf8EBAMCAYYwEwYDVR0lBAwwCgYIKwYBBQUHAwMweQYIKwYBBQUH
+# AQEEbTBrMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5jb20wQwYI
+# KwYBBQUHMAKGN2h0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFz
+# c3VyZWRJRFJvb3RDQS5jcnQwgYEGA1UdHwR6MHgwOqA4oDaGNGh0dHA6Ly9jcmw0
+# LmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RDQS5jcmwwOqA4oDaG
+# NGh0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RD
+# QS5jcmwwTwYDVR0gBEgwRjA4BgpghkgBhv1sAAIEMCowKAYIKwYBBQUHAgEWHGh0
+# dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9DUFMwCgYIYIZIAYb9bAMwHQYDVR0OBBYE
+# FFrEuXsqCqOl6nEDwGD5LfZldQ5YMB8GA1UdIwQYMBaAFEXroq/0ksuCMS1Ri6en
+# IZ3zbcgPMA0GCSqGSIb3DQEBCwUAA4IBAQA+7A1aJLPzItEVyCx8JSl2qB1dHC06
+# GsTvMGHXfgtg/cM9D8Svi/3vKt8gVTew4fbRknUPUbRupY5a4l4kgU4QpO4/cY5j
+# DhNLrddfRHnzNhQGivecRk5c/5CxGwcOkRX7uq+1UcKNJK4kxscnKqEpKBo6cSgC
+# PC6Ro8AlEeKcFEehemhor5unXCBc2XGxDI+7qPjFEmifz0DLQESlE/DmZAwlCEIy
+# sjaKJAL+L3J+HNdJRZboWR3p+nRka7LrZkPas7CM1ekN3fYBIM6ZMWM9CBoYs4Gb
+# T8aTEAb8B4H6i9r5gkn3Ym6hU/oSlBiFLpKR6mhsRDKyZqHnGKSaZFHvMIIFVTCC
+# BD2gAwIBAgIQDOzRdXezgbkTF+1Qo8ZgrzANBgkqhkiG9w0BAQsFADByMQswCQYD
+# VQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGln
+# aWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQgQ29k
+# ZSBTaWduaW5nIENBMB4XDTIwMDYxNDAwMDAwMFoXDTIzMDYxOTEyMDAwMFowgZEx
+# CzAJBgNVBAYTAkFVMRgwFgYDVQQIEw9OZXcgU291dGggV2FsZXMxFDASBgNVBAcT
+# C0NoZXJyeWJyb29rMRowGAYDVQQKExFEYXJyZW4gSiBSb2JpbnNvbjEaMBgGA1UE
+# CxMRRGFycmVuIEogUm9iaW5zb24xGjAYBgNVBAMTEURhcnJlbiBKIFJvYmluc29u
+# MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwj7PLmjkknFA0MIbRPwc
+# T1JwU/xUZ6UFMy6AUyltGEigMVGxFEXoVybjQXwI9hhpzDh2gdxL3W8V5dTXyzqN
+# 8LUXa6NODjIzh+egJf/fkXOgzWOPD5fToL7mm4JWofuaAwv2DmI2UtgvQGwRhkUx
+# Y3hh0+MNDSyz28cqExf8H6mTTcuafgu/Nt4A0ddjr1hYBHU4g51ZJ96YcRsvMZSu
+# 8qycBUNEp8/EZJxBUmqCp7mKi72jojkhu+6ujOPi2xgG8IWE6GqlmuMVhRSUvF7F
+# 9PreiwPtGim92RG9Rsn8kg1tkxX/1dUYbjOIgXOmE1FAo/QU6nKVioJMNpNsVEBz
+# /QIDAQABo4IBxTCCAcEwHwYDVR0jBBgwFoAUWsS5eyoKo6XqcQPAYPkt9mV1Dlgw
+# HQYDVR0OBBYEFOh6QLkkiXXHi1nqeGozeiSEHADoMA4GA1UdDwEB/wQEAwIHgDAT
+# BgNVHSUEDDAKBggrBgEFBQcDAzB3BgNVHR8EcDBuMDWgM6Axhi9odHRwOi8vY3Js
+# My5kaWdpY2VydC5jb20vc2hhMi1hc3N1cmVkLWNzLWcxLmNybDA1oDOgMYYvaHR0
+# cDovL2NybDQuZGlnaWNlcnQuY29tL3NoYTItYXNzdXJlZC1jcy1nMS5jcmwwTAYD
+# VR0gBEUwQzA3BglghkgBhv1sAwEwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cu
+# ZGlnaWNlcnQuY29tL0NQUzAIBgZngQwBBAEwgYQGCCsGAQUFBwEBBHgwdjAkBggr
+# BgEFBQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tME4GCCsGAQUFBzAChkJo
+# dHRwOi8vY2FjZXJ0cy5kaWdpY2VydC5jb20vRGlnaUNlcnRTSEEyQXNzdXJlZElE
+# Q29kZVNpZ25pbmdDQS5jcnQwDAYDVR0TAQH/BAIwADANBgkqhkiG9w0BAQsFAAOC
+# AQEANWoHDjN7Hg9QrOaZx0V8MK4c4nkYBeFDCYAyP/SqwYeAtKPA7F72mvmJV6E3
+# YZnilv8b+YvZpFTZrw98GtwCnuQjcIj3OZMfepQuwV1n3S6GO3o30xpKGu6h0d4L
+# rJkIbmVvi3RZr7U8ruHqnI4TgbYaCWKdwfLb/CUffaUsRX7BOguFRnYShwJmZAzI
+# mgBx2r2vWcZePlKH/k7kupUAWSY8PF8O+lvdwzVPSVDW+PoTqfI4q9au/0U77UN0
+# Fq/ohMyQ/CUX731xeC6Rb5TjlmDhdthFP3Iho1FX0GIu55Py5x84qW+Ou+OytQcA
+# FZx22DA8dAUbS3P7OIPamcU68TGCAigwggIkAgEBMIGGMHIxCzAJBgNVBAYTAlVT
+# MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+# b20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25p
+# bmcgQ0ECEAzs0XV3s4G5ExftUKPGYK8wCQYFKw4DAhoFAKB4MBgGCisGAQQBgjcC
+# AQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYB
+# BAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFO+7xJQX2Xbk
+# ao33dgltH02k7ErbMA0GCSqGSIb3DQEBAQUABIIBAKoUIBYS6ITW5qvetC+dxmiw
+# u/8NiK9vVNVMCb+xup5UVJmeHGb/RJDYXNhe2/KvebpUXEscwXS4gkq98/UayiA4
+# RyDVBFuMhq5LzF9V8T5u7I0e35HJATaNOOIKxxbNVGGaDQj2SLMNGCIQopJG3Xdc
+# aySZj+3btDm/KugCRygW9OyAzEJAcQfo2kHjH2QDZayv8X/lVbBOUnWxS4PFdSo4
+# HaBtpgVUilE/mvmnrCw4cTcsJ9xh9wvGEt+tuGdXOPgSoHIhcCCalv7O5Zr0JgJI
+# 0tIBwX4chEDICLMPUkl1TWePppNYHz5+90BmmyMknV/+aD9MRo5EtrP9imOfwWI=
+# SIG # End signature block
---
+++ README.md
@@ -0,0 +1,96 @@
+# PKCE Code Verifier and Challenge Generator PowerShell Module
+Generate OAuth 2.0 Proof Key for Code Exchange (PKCE) 'code_challenge' and 'code_verifier' for use with an OAuth2 Authorization Code Grant flow. 
+
+[![PSGallery Version](https://img.shields.io/powershellgallery/v/PKCE.svg?style=flat&logo=powershell&label=PSGallery%20Version)](https://www.powershellgallery.com/packages/PKCE) [![PSGallery Downloads](https://img.shields.io/powershellgallery/dt/PKCE.svg?style=flat&logo=powershell&label=PSGallery%20Downloads)](https://www.powershellgallery.com/packages/PKCE)
+
+It contains one cmdlet (New-PKCE) that does a number of key things;
+* Generate both the code_verifier and the associated code_challenge
+* Generate the code_challenge for a valid code_verifier
+* Generate a code_verifier of a specified length [as per RFC 7636](https://datatracker.ietf.org/doc/html/rfc7636)
+
+[Available in the PowerShell Gallery](https://www.powershellgallery.com/packages/PKCE)
+
+[Associated Blogpost](https://blog.darrenjrobinson.com/?s=pkce)
+
+## Install
+Install direct from the PowerShell Gallery (Powershell 5.1 and above).
+
+
+```powershell
+install-module -name PKCE
+```
+
+## Import
+Import the Module. 
+
+```powershell
+import-module PKCE
+```
+
+## Example 
+Generate a code_verifier and code_challenge using the default 127 character length. 
+
+```powershell
+New-PKCE | FL
+```
+
+### Output 
+```
+code_verifier  : WoBOriV4tDoK61KVPbWM71sk5maozuSXjxRUO2fu9zIQCoH0ExenrT7clYznp15CKLy8HnnXENGBw3SuZjM0T2vaUvVJBD7ThpD2XqkjdPyekAkYFs8b4cCFaxHgFOhO
+code_challenge : dKk5vCznfP9dqr7PPugOfq/cmyX1hEMzvdVT/x0ZRg0
+```
+
+## Example 
+Generate a code_verifier and code_challenge with a length of 99 for the code_verifier.
+
+```powershell 
+New-PKCE -length 99 | FL
+```
+
+### Output
+
+```
+code_verifier  : yf82tm6q0Q9mgkFfxPXfRNyN4dhlDHrI9kKKsX5vAVhMLeW80LyutH3wx9bPh82wisluIBOsaR6Z7P0z5LMcqoOfJRayn7ZpTkD
+code_challenge : YOamaHC/iqRrIkSusU2FEMahO2BVJz1KZzEQ9o6j5kE
+```
+
+## Example 
+Generate a code_challenge for a specified code_verifier.
+
+```powershell 
+New-PKCE -codeVerifier '0_EUVyNBjAFIAKg7xqw-3WC5xAZkx..~~xqndi4WRc1NCCEdXUScvCfDsVygmDQQ~eIZiVamZsQp3XhqL9TuH5~9U-BRBvI3KeOtalT.~uByhfTcGhnd9gHySopRaeLN' | FL
+```
+
+### Output
+
+```
+code_verifier  : 0_EUVyNBjAFIAKg7xqw-3WC5xAZkx..~~xqndi4WRc1NCCEdXUScvCfDsVygmDQQ~eIZiVamZsQp3XhqL9TuH5~9U-BRBvI3KeOtalT.~uByhfTcGhnd9gHySopRaeLN
+code_challenge : gewJaAUliqSe-nMhl48sutkX4ayfCtcPc-72jRnAPMA
+```
+
+## Example 
+Generate a code_challenge and code_verifier and assign to variables for use in an OAuth2 Authorization Code Grant flow
+
+```powershell 
+$pkceCodes = New-PKCE 
+$codeChallenge = $pkceCodes.code_challenge
+$codeVerifier = $pkceCodes.code_verifier
+
+$pkceCodes | FL
+$codeVerifier
+$codeChallenge
+```
+
+### Output
+
+```
+code_verifier  : n8bOz65bchiqsu5dW1JTRWBGWlkbmKUCXR5CRiUqrdqIBeSvTlOjS8i9xsgpVVMBXEgjNKDBKhFNDnzFa4yp87v3fZNgPA2MtFEIrtdjoRkvtmAwrj3uCcyf1A4h7ZGY
+code_challenge : T1nH0TteC6QN7p4upQ4GPT/x0nWCfKMrtTi5+mptrvg
+
+n8bOz65bchiqsu5dW1JTRWBGWlkbmKUCXR5CRiUqrdqIBeSvTlOjS8i9xsgpVVMBXEgjNKDBKhFNDnzFa4yp87v3fZNgPA2MtFEIrtdjoRkvtmAwrj3uCcyf1A4h7ZGY
+T1nH0TteC6QN7p4upQ4GPT/x0nWCfKMrtTi5+mptrvg
+```
+
+## Keep up to date
+* [Visit my blog](http://darrenjrobinson.com/)
+* ![](http://twitter.com/favicon.ico) [Follow on Twitter](https://twitter.com/darrenjrobinson)
